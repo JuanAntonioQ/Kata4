6112c015ef3cffdb3a3165775a4185c40feee132
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-11-28 23:13:40 +0000

    Second version

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,19 +1,41 @@
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+ 
 public class Kata4 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         String path = "C:\\Users\\Juan Antonio\\Downloads";
         File file = new File(path);
         
         String[] fileList = file.list();
         
-        /*for (String fileList1 : fileList) {
-            System.out.println(fileList1);
-        }*/
+        String from = "C:\\Users\\Juan Antonio\\Downloads\\p1.pdf";
+        String to = "C:\\Users\\Juan Antonio\\Downloads\\p1(1).pdf";
         
-        print(file.listFiles(), "");
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File (to)));
+     
+        byte[] buffer = new byte[1024];
+        
+        Date date = new Date();
+        
+        while (true){
+            int read = input.read(buffer);
+            if (read < 0){
+                break;
+            }
+            output.write(buffer);
+        }
+        System.out.println(new Date().getTime()-date.getTime() + "msg");
+        input.close();
+        output.flush();
+        output.close();
     }
     
     private static void print(File[] listFiles, String ident){
@@ -22,7 +44,7 @@ public class Kata4 {
             return;
         }
         
-        for ( File listFile : listFiles){
+        for (File listFile : listFiles){
             System.out.println(ident + (listFile.isDirectory()? "+" : " ") + listFile.getName());
             if(listFile.isFile() || listFile.isHidden()) continue;
                 print (listFile.listFiles(), ident + " ");
