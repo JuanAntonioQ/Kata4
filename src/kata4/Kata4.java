dccc1f23bd42ec4cfa9d4248d5e3c3391ec700ff
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2016-01-06 17:03:10 +0000

    Latest Version

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -3,7 +3,7 @@ package kata4;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
- 
+
 public class Kata4 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String name = "C:\\Users\\Juan Antonio\\Documents\\NetBeansProjects\\Kata4\\DATA\\emails.txt";
